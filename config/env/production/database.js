8d5fee2830364c1524f467228763bffa37b543cf
Abish Vijayan <abishvijayan7643@gmail.com>
2022-04-13 17:10:10 +0530

    [Bug][Fixed] Environment Variable Not String

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,11 +8,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env(DATABASE_HOST, "127.0.0.1"),
-        port: env(DATABASE_PORT, "27017"),
-        database: env(DATABASE_NAME, "strapi"),
-        username: env(DATABASE_USERNAME, ""),
-        password: env(DATABASE_PASSWORD, ""),
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env("DATABASE_PORT", "27017"),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
         ssl: {
           rejectUnauthorized: false,
         },
